eff664a918d1287a5d5df3e9fae95a00c13bb8d2
Takeru Ohta <phjgt308@gmail.com>
2015-02-03 22:10:18 +0900

    Implement first version of rebar_applications_plugin

--- src/rebar_applications_plugin.erl
+++ src/rebar_applications_plugin.erl
@@ -13,19 +13,19 @@
 %%----------------------------------------------------------------------------------------------------------------------
 %% Macros
 %%----------------------------------------------------------------------------------------------------------------------
--define(OPTION(Key, Config, Default), proplists:get_value(Key, rebar_config:get_local(Config, fill_apps_opts, []), Default)).
--define(DEBUG(Format, Args), rebar_log:log(debug, "[~p:~s:~p] "++Format++"~n", [self(), ?MODULE, ?LINE | Args])).
+-define(OPTION(Key, Config, Default), proplists:get_value(Key, rebar_config:get(Config, fill_apps_opts, []), Default)).
+-define(DEBUG(Format, Args), rebar_log:log(debug, "[~s:~p] "++Format++"~n", [?MODULE, ?LINE | Args])).
+-define(XREF_SERVER, ?MODULE).
+-define(APPS_CACHE_KEY, {?MODULE, apps_cache}).
 
 %%----------------------------------------------------------------------------------------------------------------------
 %% Exported Functions
 %%----------------------------------------------------------------------------------------------------------------------
 %% @doc 対象アプリケーションの実際の依存関係を解析して、アプリケーションリソースファイルの`applications'項目を自動生成する
 -spec 'fill-apps'(rebar_config:config(), filename:name()) -> ok | {ok, rebar_config:config()}.
-'fill-apps'(_Config, undefined) ->
-    ok; % アプリケーションリソースファイルが存在しない場合は対象外
 'fill-apps'(Config00, AppFile) ->
-    case rebar_app_utils:is_app_src(AppFile) of
-        false -> ok;
+    case AppFile =/= undefined andalso rebar_app_utils:is_app_src(AppFile) of
+        false -> ok; % `AppFile'が"*.app.src"ではない場合は対象外
         true  ->
             ?DEBUG("appfile=~s", [AppFile]),
 
@@ -34,8 +34,8 @@
             {Config2, Applications0} = rebar_app_utils:app_applications(Config1, AppFile),
             ?DEBUG("appname=~s", [AppName]),
 
-            Applications1 = collect_direct_depending_applications(AppName, AppFile, Config2),
-            Applications2 = update_applications(Applications0, Applications1),
+            Applications1 = collect_direct_depending_applications(AppName, Config2),
+            Applications2 = merge_applications(Applications0, Applications1),
             ?DEBUG("apps: original=~w, collected=~w, result=~w", [Applications0, Applications1, Applications2]),
 
             ok = rewrite_applications(AppFile, Applications2),
@@ -45,14 +45,20 @@
 %%----------------------------------------------------------------------------------------------------------------------
 %% Internal Functions
 %%----------------------------------------------------------------------------------------------------------------------
+%% @doc xrefサーバを起動して、依存関係解析のための下準備を整える
+%%
+%% 毎回、準備処理が走るのを避けるために、このサーバは一度起動したらそのままにしておく(rebarの終了時に自動で落ちる)
+-spec init_xref(rebar_config:config()) -> rebar_config:config().
 init_xref(Config0) ->
-    case xref:start(?MODULE, [{xref_mode, modules}]) of
+    case xref:start(?XREF_SERVER, [{xref_mode, modules}]) of
         {error, {already_started, Xref}} ->
-            ?DEBUG("xref is already started: pid=~p, name=~s", [Xref, ?MODULE]),
+            ?DEBUG("xref is already started: pid=~p", [Xref]),
             Config0;
         {ok, Xref} ->
-            ?DEBUG("start new xref: pid=~p, name=~s", [Xref, ?MODULE]),
+            ?DEBUG("start new xref: pid=~p", [Xref]),
 
+            %% `{include_system_apps, false}'がオプションで指定されている場合は、システムライブラリ群は解析対象に含めない
+            %% (所要時間が長くなるので、デフォルトは`false')
             SystemLibDir = code:lib_dir(),
             IncludeSystemApps = ?OPTION(include_system_apps, Config0, false),
 
@@ -78,33 +84,54 @@ init_xref(Config0) ->
             Config1
     end.
 
--spec collect_direct_depending_applications(atom(), filename:name(), rebar_config:config()) -> [atom()].
-collect_direct_depending_applications(AppName, AppFile, Config) ->
-    {ok, Calls0} = xref:analyze(?MODULE, {application_call, AppName}), % XXX: MODULE
-    Calls = ordsets:subtract(ordsets:from_list(Calls0), get_subapps(Config)),
-    case rebar_app_utils:is_app_dir() of
-        {true, AppFile} -> lists:delete(AppName, Calls ++ get_subapps(Config)); % root application
-        {true, _}       -> lists:delete(AppName, Calls)
+%% @doc xrefを使って`AppName'が直接依存(使用)しているアプリケーション群を取得する
+-spec collect_direct_depending_applications(AppName, rebar_config:config()) -> ordsets:ordset(AppName) when
+      AppName :: atom().
+collect_direct_depending_applications(AppName, Config) ->
+    {ok, Calls} = xref:analyze(?XREF_SERVER, {application_call, AppName}),
+    case rebar_utils:processing_base_dir(Config) of
+        true ->
+            %% ルートアプリケーションは全てのサブアプリケーションを依存に含める
+            Includes = get_root_and_subapps(Config),
+            Excludes = [AppName],
+            ?DEBUG("includes: ~w", [Includes]),
+            ordsets:subtract(ordsets:union(Calls, Includes), Excludes);
+        false ->
+            %% ルート以外は、循環参照の可能性を減らすために、サブアプリケーションおよびルートを依存に含めないようにする
+            %% (どうしても含めたい場合は、"*.app.src"に直接指定すること)
+            Excludes = ordsets:add_element(AppName, get_root_and_subapps(Config)),
+            ?DEBUG("excludes: ~w", [Excludes]),
+            ordsets:subtract(Calls, Excludes)
     end.
 
-%% TODO: cache
-get_subapps(Config) ->
-    Dirs = rebar_config:get_local(Config, sub_dirs, []),
-    ordsets:from_list(
-      lists:filtermap(
-        fun (Dir) ->
-                case rebar_app_utils:is_app_dir(Dir) of
-                    false           -> false;
-                    {true, AppFile} ->
-                        {_, AppName} = rebar_app_utils:app_name(Config, AppFile),
-                        {true, AppName}
-              end
-        end,
-        Dirs)).
+-spec get_root_and_subapps(rebar_config:config()) -> ordsets:ordset(atom()).
+get_root_and_subapps(Config) ->
+    case get(?APPS_CACHE_KEY) of
+        undefined ->
+            BaseDir = rebar_utils:base_dir(Config),
+            Dirs = ["" | rebar_config:get(Config, sub_dirs, [])],
+            Apps =
+                ordsets:from_list(
+                  lists:filtermap(
+                    fun (Dir) ->
+                            case rebar_app_utils:is_app_dir(filename:join(BaseDir, Dir)) of
+                                false           -> false;
+                                {true, AppFile} ->
+                                    {_, AppName} = rebar_app_utils:app_name(Config, AppFile),
+                                    {true, AppName}
+                            end
+                    end,
+                    Dirs)),
+            put(?APPS_CACHE_KEY, Apps), % キャッシュする
+            Apps;
+        Apps -> Apps
+    end.
 
-update_applications(Apps0, Apps1) ->
-    Apps0 ++ lists:foldl(fun lists:delete/2, Apps1, Apps0).
+-spec merge_applications([atom()], ordsets:ordset(atom())) -> [atom()].
+merge_applications(OrignalApps, GeneratedApps) ->
+    OrignalApps ++ ordsets:subtract(GeneratedApps, ordsets:from_list(OrignalApps)).
 
+-spec rewrite_applications(filename:name(), [atom()]) -> ok | {error, Reason::term()}.
 rewrite_applications(AppFile, Applications) ->
     EbinAppFile = rebar_app_utils:app_src_to_app(AppFile),
     {ok, [{application, AppName, AppKeys0}]} = file:consult(EbinAppFile),
